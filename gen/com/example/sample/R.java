9389b4034d7f1317c594683ffb221ffe24ce5241
c243zhan <zc632995195@gmail.com>
2013-10-21 23:18:19 -0400

    Main Menu Views

--- gen/com/example/sample/R.java
+++ gen/com/example/sample/R.java
@@ -16,7 +16,15 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        /**  splash screen 
+         */
         public static final int logo_color=0x7f050000;
+        public static final int menu_color=0x7f050005;
+        public static final int menu_glow=0x7f050006;
+        /**  menu screen 
+         */
+        public static final int title_color=0x7f050003;
+        public static final int title_glow=0x7f050004;
         public static final int version_bkgrd=0x7f050002;
         public static final int version_color=0x7f050001;
     }
@@ -26,51 +34,72 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060003;
-        /**  Default screen margins, per the Android Design guidelines. 
+        public static final int activity_horizontal_margin=0x7f060005;
+        /**  splash screen 
          */
         public static final int logo_size=0x7f060000;
+        public static final int menu_item_size=0x7f060004;
+        /**  menu screen 
+         */
+        public static final int screen_title_size=0x7f060003;
         public static final int version_size=0x7f060001;
         public static final int version_spacing=0x7f060002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int splash1=0x7f020002;
-        public static final int splash2=0x7f020003;
-        public static final int splash3=0x7f020004;
-        public static final int splash4=0x7f020005;
+        public static final int bkgrnd=0x7f020000;
+        public static final int divider=0x7f020001;
+        public static final int half=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int selector=0x7f020005;
+        public static final int splash1=0x7f020006;
+        public static final int splash2=0x7f020007;
+        public static final int splash3=0x7f020008;
+        public static final int splash4=0x7f020009;
     }
     public static final class id {
-        public static final int ImageView2_Left=0x7f090003;
-        public static final int ImageView2_Right=0x7f090004;
-        public static final int ImageView3_Left=0x7f090006;
-        public static final int ImageView4_Right=0x7f090007;
-        public static final int TableLayout01=0x7f090001;
-        public static final int TableRow01=0x7f090002;
-        public static final int TableRow02=0x7f090005;
-        public static final int TextViewBottomTitle=0x7f090008;
-        public static final int TextViewBottomVersion=0x7f090009;
-        public static final int TextViewTopTitle=0x7f090000;
+        public static final int ImageView2_Left=0x7f090009;
+        public static final int ImageView2_Right=0x7f09000a;
+        public static final int ImageView3_Left=0x7f09000c;
+        public static final int ImageView4_Right=0x7f09000d;
+        public static final int ImageView_MenuBottom=0x7f090005;
+        public static final int ImageView_MenuHeader=0x7f090001;
+        public static final int ImageView_MenuHeader2=0x7f090003;
+        public static final int ListView_Menu=0x7f090004;
+        public static final int RelativeLayout01=0x7f090000;
+        public static final int TableLayout01=0x7f090007;
+        public static final int TableRow01=0x7f090008;
+        public static final int TableRow02=0x7f09000b;
+        public static final int TextView01=0x7f090002;
+        public static final int TextViewBottomTitle=0x7f09000e;
+        public static final int TextViewBottomVersion=0x7f09000f;
+        public static final int TextViewTopTitle=0x7f090006;
     }
     public static final class layout {
         public static final int game=0x7f030000;
         public static final int help=0x7f030001;
         public static final int menu=0x7f030002;
-        public static final int scores=0x7f030003;
-        public static final int settings=0x7f030004;
-        public static final int splash=0x7f030005;
+        public static final int menu_item=0x7f030003;
+        public static final int scores=0x7f030004;
+        public static final int settings=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class string {
         public static final int app_logo_bottom=0x7f070002;
         public static final int app_logo_top=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int app_version_info=0x7f070003;
-        public static final int game=0x7f070005;
-        public static final int help=0x7f070008;
-        public static final int menu=0x7f070004;
-        public static final int scores=0x7f070006;
-        public static final int settings=0x7f070007;
+        public static final int game=0x7f070004;
+        public static final int help=0x7f070007;
+        /**  menu screen 
+         */
+        public static final int menu=0x7f070008;
+        public static final int menu_item_help=0x7f07000c;
+        public static final int menu_item_play=0x7f07000a;
+        public static final int menu_item_scores=0x7f07000b;
+        public static final int menu_item_settings=0x7f070009;
+        public static final int scores=0x7f070005;
+        public static final int settings=0x7f070006;
     }
     public static final class style {
         /** 
